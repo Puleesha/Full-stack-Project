fd9b6241d3616b64223dfb0d8a2885b85b04ed9e
Puleesha Arachchi <puleeshak@gmail.com>
2024-03-18 16:36:20 +0530

    Added JSON object

--- backend/customers/app.js
+++ backend/customers/app.js
@@ -18,10 +18,36 @@
 
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 const PORT = 3000;
+
+const customers = [
+    {
+        "name" : "Caleb",
+        "industry" : "music"
+    },
+    {
+        "name" : "John",
+        "industry" : "networking"
+    },
+    {
+        "name" : "Sal",
+        "industry" : "sports medicine"
+    }
+]
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+})
+
+app.get('/api/customers', (req, res) => {
+    res.send({"customers" : customers});
+})
+
+app.post('/api/customers', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 })
 
 app.post('/', (req, res) => {
